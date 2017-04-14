d263dfaa798eb8796d4f077b3764a2c8cf46c0da
Aldwin Vlasblom <aldwin.vlasblom@gmail.com>
2017-04-14 10:26:36 +0200

    Correct invalid link

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 //.
 //. ### Specification
 //.
-//. For an algebraic data type to be compatible with the [algorithm][3]:
+//. For an algebraic data type to be compatible with the algorithm:
 //.
 //.   - every member of the type MUST have a `constructor` property pointing
 //.     to an object known as the _type representative_;
